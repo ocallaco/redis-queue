5d1e0fba17e1d33805c2b048d1bd510606b8c800
Conall <=>
2014-01-22 10:57:54 -0500

    new tests

---
+++ test/test_lbexit.lua
@@ -0,0 +1,34 @@
+local tester = require './consume_template'
+
+tester.prepareEnvironment = function()
+   tester.jobsSeenBy = {}
+   tester.jobsSeen = {}
+end
+
+tester.prepareWorker = function(worker)
+   tester.jobsSeenBy[worker.index] = {}
+end
+
+tester.generateJob = function(worker)
+   local i = worker.index
+   local jobDescriptor = {USER = {
+      testJob = function(args) 
+         os.exit()
+      end}
+   }
+   return jobDescriptor
+end
+
+tester.evaluateCode = function(i)
+   print("jobs seen by worker " .. i .. ": " .. #tester.jobsSeenBy[i])
+
+   if i == 1 then
+      for j = 1,25 do
+         if not tester.jobsSeen[j] then
+            print("!!!!!!!!!!!!Missed job " .. j)
+         end
+      end
+   end
+end
+
+tester.run()
